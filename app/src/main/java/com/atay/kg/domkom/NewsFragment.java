5e83d753228ad8d2fd449bb1f8d586ad1264c82d
Ataykano <atai.karagulov@iaau.edu.kg>
2020-05-08 11:38:52 +0600

    12

--- app/src/main/java/com/atay/kg/domkom/NewsFragment.java
+++ app/src/main/java/com/atay/kg/domkom/NewsFragment.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.atay.kg.domkom.Adapters.NewsAdapter;
+import com.atay.kg.domkom.Interfaces.RequestInterface;
+import com.atay.kg.domkom.Models.NewsModel;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,17 +28,17 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class NewsFragment extends Fragment {
 
-    private final String JSON_URL = "https://dashboard.heroku.com/apps/domkom-app/news-api/";
-    private RecyclerView myrecyclerview;
-    private RequestQueue requestQueue;
-    private JsonArrayRequest request;
-    private List<NewsClass> listNews;
-
-    public NewsFragment() {
-
-    }
+    ArrayList<NewsModel> newsModels = new ArrayList<>();
+    private NewsAdapter newsAdapter;
+    private RecyclerView news_recyclerview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,69 +46,40 @@ public class NewsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_news, container, false);
 
+        news_recyclerview = (RecyclerView)view.findViewById(R.id.NewsRecyclerView);
+        news_recyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        news_recyclerview.setHasFixedSize(true);
+        news_recyclerview.setAdapter(newsAdapter);
 
-       /* listNews = new ArrayList<>() ;
-        jsonrequest();
+        getNewsResponse();
 
-*/
-        return view;
-
+    return view;
     }
-}
 
-   /* private void jsonrequest() {
-        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
+    private void getNewsResponse() {
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl("https://dashboard.heroku.com/apps/domkom-app/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RequestInterface requestInteface=retrofit.create(RequestInterface.class);
+        Call<List<NewsModel>> call=requestInteface.getNewsJson();
+
+
+        call.enqueue(new Callback<List<NewsModel>>() {
             @Override
-            public void onResponse(JSONArray response) {
-
-                JSONObject jsonObject  = null ;
-
-                for (int i = 0 ; i < response.length(); i++ ) {
-
-
-                    try {
-                        jsonObject = response.getJSONObject(i) ;
-                        NewsClass newsClass = new NewsClass() ;
-                        newsClass.setTitle(jsonObject.getString("title"));
-                        newsClass.setText(jsonObject.getString("text"));
-                        newsClass.setDate(jsonObject.getString("date"));
-                        newsClass.setId(jsonObject.getInt("id"));
-                        newsClass.setImage_url(jsonObject.getString("image"));
-                        listNews.add(newsClass);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-
-                }
-
-                setuprecyclerview(listNews);
-
+            public void onResponse(Call<List<NewsModel>> call, Response<List<NewsModel>> response) {
+                newsModels = new ArrayList<>(response.body());
+                newsAdapter = new NewsAdapter(NewsFragment.this,newsModels);
+                news_recyclerview.setAdapter(newsAdapter);
+                Toast.makeText(getActivity(), "Успешно", Toast.LENGTH_SHORT).show();
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
 
+            @Override
+            public void onFailure(Call<List<NewsModel>> call, Throwable t) {
+                Toast.makeText(getActivity(),"Ошибка",Toast.LENGTH_SHORT).show();
             }
         });
-
-
-        requestQueue = (RequestQueue) Volley.newRequestQueue(NewsFragment.this);
-        requestQueue.add(request) ;
-*//**//*
-
     }
 
-    private void setuprecyclerview(List<NewsClass> listNews) {
-
-
-        RycyclerViewAdapter myadapter = new RycyclerViewAdapter(this,listNews);
-        myrecyclerview.setLayoutManager(new LinearLayoutManager(this));
-        myrecyclerview.setAdapter(myadapter);
-
-    }
 }
-
-
-
-*/
